f301c774ec205890e03de53a79e75351721e00c4
= <=>
2024-05-20 10:34:44 -0400

    Readd index.php to serve python scraper

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Execute the Python script and capture its output
+$output = shell_exec("python3 lme-scraper.py");
+
+// Decode the output as JSON
+$data = json_decode($output);
+
+// Return the JSON data
+header('Content-Type: application/json');
+echo json_encode($data);
+
+?>
